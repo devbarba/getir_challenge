550569515363380f3f8c4f97eed5f6bc75cee103
Joao Harbs <harbspj@gmail.com>
2022-02-11 19:23:07 +0100

    chore: adding project configs

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-import { pathsToModuleNameMapper } from 'ts-jest/utils';
-import { compilerOptions } from './tsconfig.json';
-
-export default {
-  clearMocks: true,
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
