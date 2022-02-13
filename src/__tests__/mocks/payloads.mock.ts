2d88fa32ce7f544b33a81aada228b4ac9d6a0744
Joao Harbs <harbspj@gmail.com>
2022-02-13 20:31:28 +0100

    fix: fixing tests end lag

---
+++ src/__tests__/mocks/payloads.mock.ts
@@ -0,0 +1,13 @@
+const defaultPayload = {
+    startDate: '2016-01-26',
+    endDate: '2018-02-02',
+    minCount: 2700,
+    maxCount: 3000,
+};
+
+const missingKeysPayload = {
+    startDate: '2016-01-26',
+    maxCount: 3000,
+};
+
+export { defaultPayload, missingKeysPayload };
