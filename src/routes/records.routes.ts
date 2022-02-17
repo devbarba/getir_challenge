671584cc405a5472e6626b95784357ce492d2627
Joao Harbs <harbspj@gmail.com>
2022-02-17 16:28:53 +0100

    refactor: added validation with joy and new verifyFields function

--- src/routes/records.routes.ts
+++ src/routes/records.routes.ts
@@ -1,12 +1,24 @@
 import { Router, Response, Request, NextFunction } from 'express';
+import Joi from 'joi';
 
 import RecordsController from '../controllers/record.controller';
+import { verifyFields } from '../utils/helper';
 
 const recordsController = new RecordsController();
 
 const recordsRoutes = Router();
 
 recordsRoutes.post('/', (req: Request, res: Response, next: NextFunction) => {
+    verifyFields(
+        req.body,
+        Joi.object({
+            startDate: Joi.date().less(Joi.ref('endDate')).required(),
+            endDate: Joi.date().required(),
+            minCount: Joi.number().less(Joi.ref('maxCount')).required(),
+            maxCount: Joi.number().required(),
+        })
+    );
+
     recordsController.read({ req, res, next });
 });
 
